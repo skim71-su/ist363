ee2a3ffd2ee2f542b7a061a9196ea057f841f12c
skim71-su <112636203+skim71-su@users.noreply.github.com>
2023-09-12 18:19:04 -0400

    attendance for 9/12/23

--- starbucks/js/main.js
+++ starbucks/js/main.js
@@ -26,3 +26,35 @@ menuBtn.addEventListener("click", function() {
 closeBtn.addEventListener("click", function() {
     overlay.classList.remove("active");
 }); //end of closeBtn click event
+
+// arrays and objects
+
+// basic variables
+const coffeeName = "Mocha"; //const can't be edited and prevents from modifying
+const coffeeHeadline = document.createElement("h2");
+coffeeHeadline.textContent = coffeeName;
+document.body.appendChild(coffeeHeadline);
+
+//objects
+
+const coffee = {
+    name: "Mocha",
+    price: 3.99,
+    description: "This is a delicious cup of coffee."
+};
+
+console.log(coffee.description); //shows "This is a delicious cup of coffee."
+const coffeeObjHeadline = document.createElement("h2");
+coffeeObjHeadline.textContent = coffee.name;
+document.body.appendChild(coffeeObjHeadline);
+
+//arrays
+
+const coffees = ["Mocha", "Latte", "Espresso"];
+console.log(coffees[0]);
+
+coffees.forEach(function(coffee) {
+    console.log(coffee);
+});
+
+
