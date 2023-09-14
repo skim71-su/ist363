211b8221baee96e1df4ecc51c28f0db696f6a77f
skim71-su <112636203+skim71-su@users.noreply.github.com>
2023-09-14 18:17:03 -0400

    attendance for 9/14/23

--- starbucks/js/main.js
+++ starbucks/js/main.js
@@ -33,7 +33,7 @@ closeBtn.addEventListener("click", function() {
 const coffeeName = "Mocha"; //const can't be edited and prevents from modifying
 const coffeeHeadline = document.createElement("h2");
 coffeeHeadline.textContent = coffeeName;
-document.body.appendChild(coffeeHeadline);
+//document.body.appendChild(coffeeHeadline);
 
 //objects
 
@@ -46,15 +46,75 @@ const coffee = {
 console.log(coffee.description); //shows "This is a delicious cup of coffee."
 const coffeeObjHeadline = document.createElement("h2");
 coffeeObjHeadline.textContent = coffee.name;
-document.body.appendChild(coffeeObjHeadline);
+//document.body.appendChild(coffeeObjHeadline);
 
 //arrays
 
-const coffees = ["Mocha", "Latte", "Espresso"];
-console.log(coffees[0]);
+const coffees = [
+    {
+        name: "Mocha",
+        price: 3.99,
+        description: "This is a delicious cup of coffee",
+        image: {
+            fileName: "coffee.jpg",
+            width: 630,
+            height: 630,
+            altText: "A cup of coffee."
+        }
+    },
+    {
+        name: "Lattee",
+        price: 3.99,
+        description: "This is a delicious cup of coffee",
+        image: {
+            fileName: "coffee.jpg",
+            width: 630,
+            height: 630,
+            altText: "A cup of coffee."
+        }
+    },
+    {
+        name: "Espresso",
+        price: 3.99,
+        description: "This is a delicious cup of coffee",
+        image: {
+            fileName: "coffee.jpg",
+            width: 630,
+            height: 630,
+            altText: "A cup of coffee."
+        }
+    }
+];
+//console.log(coffees[0]);
 
-coffees.forEach(function(coffee) {
-    console.log(coffee);
-});
+coffees.forEach(function(coffee) { //forEach is for especially for arrays
+    // deconstruct the coffee object
+    const { name, price, description, image } = coffee; //converting to local variable
+//2. create the html elements
+    const coffeeArticle = document.createElement("article");
+
+    const coffeeImage = document.createElement("img");
+    coffeeImage.src = `images/${image.fileName}`;
+    coffeeImage.width= image.width;
+    coffeeImage.height=image.height;
+    coffeeImage.alt=image.altText;
+
+    const coffeeName = document.createElement("h2");
+    coffeeName.textContent = name;
+
+    const coffeePrice = document.createElement("h3");
+    coffeePrice.textContent = price;
+
+    const coffeeDes = document.createElement("p");
+    coffeeDes.textContent = description;
+
+//append the elemetns to the parent article
+    coffeeArticle.appendChild(coffeeImage);
+    coffeeArticle.appendChild(coffeeName);
+    coffeeArticle.appendChild(coffeePrice);
+    coffeeArticle.appendChild(coffeeDes);
+//append the article to the body
+    document.body.appendChild(coffeeArticle);
+}); // end of coffees forEach method - you can iterate using arrays
 
 
